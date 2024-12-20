718d2d9550bf8646ae6174654557bc75b6f26644
MaibHassin <maibhassin@gmail.com>
2024-12-20 22:55:48 +0100

    Clean up init.lua

--- init.lua
+++ init.lua
@@ -5,18 +5,3 @@ vim.cmd("set shiftwidth=4")
 vim.g.mapleader = " "
 
 require("config.lazy")
-require("catppuccin").setup({
-    flavour = "mocha"
-})
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-    ensure_installed = {"lua", "javascript"},
-    highlight = { enable = true },
-    indent = { enable = true }
-})
-
-vim.cmd.colorscheme "catppuccin"
